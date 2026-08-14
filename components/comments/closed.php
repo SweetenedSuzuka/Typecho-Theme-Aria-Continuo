8b39ace4f1fa5457933f27bb07f6d80d444e6b47
SweetenedSuzuka <188338189+SweetenedSuzuka@users.noreply.github.com>
2026-08-14 15:41:03 +0800

    修复：修正了closed.php中<i>标签的拼写错误

--- components/comments/closed.php
+++ components/comments/closed.php
@@ -1,5 +1,5 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $commentClosedText = isset($commentsViewData['closedText']) ? (string) $commentsViewData['closedText'] : ''; ?>
 <?php if ($commentClosedText !== ''): ?>
-    <span style="font-size: 20px;display: block;user-select: none;"><i class="iconfont icon-aria-close" sytle="font-size:20px"></i> <?php echo htmlspecialchars($commentClosedText, ENT_QUOTES, 'UTF-8'); ?></span>
+    <span style="font-size: 20px;display: block;user-select: none;"><i class="iconfont icon-aria-close" style="font-size:20px"></i> <?php echo htmlspecialchars($commentClosedText, ENT_QUOTES, 'UTF-8'); ?></span>
 <?php endif; ?>
